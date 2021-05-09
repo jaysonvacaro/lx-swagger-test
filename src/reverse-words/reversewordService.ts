c4e52a579cadb60471ead15bc15e864308004500
Jayson Vacaro <jayson.vacaro@gmail.com>
2021-05-09 15:41:52 +0800

    feat: sort & reverse logics

--- src/reverse-words/reversewordService.ts
+++ src/reverse-words/reversewordService.ts
@@ -3,10 +3,46 @@ import { Reverseword } from "./reverseword";
 export class ReversewordService {
   public get(sentence: string): Reverseword {
 
-  	sentence = "Append String";
+  	const splittedWords:string[] = sentence.split(" ");
+
+  	const result = splittedWords.map(word => {
+
+  		let splittedWord:string[] = word.split("");
+  		let reversedCharacter:string[] = splittedWord
+	          .filter(e => !e.match(/^([!,?,\,,\.]+)$/))
+	          .reverse();
+
+	    let specCharacterIndexes: Array<any> = this.getSpecCharacterIndexes(splittedWord);
+
+	    return this.insertSpecCharacters(specCharacterIndexes, reversedCharacter);
+  	});
+
+  	sentence = result.join("");
 
     return {
     	sentence
     };
+  }
+
+  private getSpecCharacterIndexes(splittedWord: string[]) {
+  	
+  	let specChar: Array<Object> = [];
+    
+    splittedWord.forEach((a, i) => {
+		if(a.match(/^([!,?,\,,\.]+)$/)) {
+			specChar.push({ index: i, value: a });
+		} 
+    });
+
+    return specChar;
+  }
+
+  private insertSpecCharacters(specCharacters: Array<any>, reversedCharacters:string[]) {
+
+  	specCharacters.forEach(item => {
+		reversedCharacters.splice(item.index, 0, item.value);
+	});
+
+	return reversedCharacters.join("");
   }
 }
--- src/sort-words/sortwordService.ts
+++ src/sort-words/sortwordService.ts
@@ -1,18 +1,53 @@
 import { Sortword } from "./sortword";
 
-export type SortwordParams = Pick<Sortword, "sentence">;
-
 export class SortwordService {
   public get(sentence: string): Sortword {
 
-  	sentence = "Append String";
+  	const splittedWords:string[] = sentence.split(" ");
 
+  	const result = splittedWords.map(word => {
 
+  		let splittedWord:string[] = word.split("");
+  		let sortedChars:string[] = splittedWord
+	          .filter(e => !e.match(/^([!,?,\,,\.]+)$/))
+	          .sort(function (a, b) {
+	              return a.toLowerCase().localeCompare(b.toLowerCase());
+	          });
 
+	    let specCharacterIndexes: Array<any> = this.getSpecCharacterIndexes(splittedWord);
 
+	    console.log(sortedChars);
+	    console.log(specCharacterIndexes);
+
+	    return this.insertSpecCharacters(specCharacterIndexes, sortedChars);
+  	});
+
+  	sentence = result.join("");
 
     return {
     	sentence
     };
+  }
+
+  private getSpecCharacterIndexes(splittedWord: string[]) {
+  	
+  	let specChar: Array<Object> = [];
+    
+    splittedWord.forEach((a, i) => {
+		if(a.match(/^([!,?,\,,\.]+)$/)) {
+			specChar.push({ index: i, value: a });
+		} 
+    });
+
+    return specChar;
+  }
+
+  private insertSpecCharacters(specCharacters: Array<any>, sortedCharacters:string[]) {
+
+  	specCharacters.forEach(item => {
+		sortedCharacters.splice(item.index, 0, item.value);
+	});
+
+	return sortedCharacters.join("");
   }
 }
